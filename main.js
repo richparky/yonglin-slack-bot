fb7b76c3afc5ac17257bd15d1a8ac73de1b0d428
Yonglin Piao <ypiao@MBP-YPIAO.local>
2019-04-01 11:25:57 +0300

    currency

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ app.get('/', function (req,res){
   };
   res.json(reply);
 });
+
 app.post('/action-endpoint', function (req, res) {
-  const challenge = req.body.challenge;
+  const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
+
   const reply = {
       "challenge": challenge
   };
-  res.json(reply);
-});
 
   const headers = {
     'Content-type': 'application/json',
@@ -30,37 +30,36 @@ app.post('/action-endpoint', function (req, res) {
 
   // console.log(req.body.event);
 
-  if (req.body.event.subtype != 'bot_message') { // se we won't reply to ourselves...
-    request.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json', function(err, res, body) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        const coindesk = JSON.parse(body);
-        const rate = coindesk.bpi.EUR.rate;
-        const reply = {
-          'channel': req.body.event.channel,
-          text: `Current BTC rate: ${rate} EUR per 1 BTC`
+if (req.body.event.subtype != 'bot_message') { // se we won't reply to ourselves...
+      request.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json', function(err, res, body) {
+        if (err) {
+          console.log(err);
         }
-
-        const options = {
-          url:   'https://slack.com/api/chat.postMessage',
-          method: 'POST',
-          headers,
-          body:  JSON.stringify(reply)
-        };
-
-        console.log(body);
-
-        request.post(options, function(err, res, body) {
-          if (err) {
-            console.log(err);
+        else {
+          const coindesk = JSON.parse(body);
+          const rate = coindesk.bpi.EUR.rate;
+          const reply = {
+            'channel': req.body.event.channel,
+            text: `Current BTC rate: ${rate} EUR per 1 BTC`
           }
-        });
-      }
-    });
-  }
 
+          const options = {
+            url:   'https://slack.com/api/chat.postMessage',
+            method: 'POST',
+            headers,
+            body:  JSON.stringify(reply)
+          };
+
+          console.log(body);
+
+          request.post(options, function(err, res, body) {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+      });
+    }
 
   res.json(reply);
 });
