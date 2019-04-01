46820222a06a6e21d1a2c20208e489f9723bc24a
Yonglin Piao <ypiao@MBP-YPIAO.local>
2019-04-01 09:38:43 +0300

    empty program

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+
+app.use(bodyParser.json());
+
+const listener = app.listen(process.env.PORT || '3000', function(){
+  console.log('Your app is listening on port ' + listener.address().port);
+});
