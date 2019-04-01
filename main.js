0802ee0291071ff3be3140abaa80a2ce24c33db7
Yonglin Piao <ypiao@MBP-YPIAO.local>
2019-04-01 11:30:58 +0300

    currency

--- main.js
+++ main.js
@@ -9,12 +9,6 @@ const listener = app.listen(process.env.PORT || '3000', function(){
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-app.get('/', function (req,res){
-  const reply = {
-    "status": "ok"
-  };
-  res.json(reply);
-});
 
 app.post('/action-endpoint', function (req, res) {
   const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
