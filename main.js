71274c4d0f58425c19d0f46afcde192f0e485f13
Yonglin Piao <ypiao@MBP-YPIAO.local>
2019-04-01 09:42:09 +0300

    add healthcheck

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ app.use(bodyParser.json());
 const listener = app.listen(process.env.PORT || '3000', function(){
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/', function (req,res){
+  const reply = {
+    "status": "ok"
+  };
+  res.json(reply);
+});
